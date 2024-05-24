f6f6128ed804133435e874f69341247898d70070
Shan-wei <500318@student.saxion.nl>
2024-05-24 20:31:54 +0200

    Main message functionality implemented

--- main.py
+++ main.py
@@ -4,5 +4,26 @@ from dotenv import load_dotenv
 from discord import Intents, Client, Message
 from responses import get_response
 
+# Step 0: Load our token from the .env file
 load_dotenv()
 TOKEN: Final = os.getenv("DISCORD_TOKEN")
+
+# Step 1: Bot setup
+intents: Intents = Intents.default()
+intents.message_content = True
+client: Client = Client(intents=intents)
+
+# Step 2: Listen for messages
+async def send_message(message: Message, user_message: str) -> None:
+    if not user_message:
+        print("(Message was empty because intents were not enabled)")
+        return
+    
+    if is_private := user_message[0] == '?':
+        user_message = user_message[1:]
+    
+    try:
+        response: str = get_response(user_message)
+        await message.author.send(response) if is_private else await message.channel.send(response)
+    except Exception as e:
+        print(e)
