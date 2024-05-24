08e025cc4918b48a7a6b2c53fc3909c0c403428d
Shan-wei <500318@student.saxion.nl>
2024-05-24 20:54:52 +0200

    The bot is alive!

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Final
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message
+from discord import Intents, Client, Message, app_commands
 from responses import get_response
 
 # Step 0: Load our token from the .env file
@@ -27,3 +27,29 @@ async def send_message(message: Message, user_message: str) -> None:
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e:
         print(e)
+
+# Step 3: Handling startup for bot
+@client.event
+async def on_ready() -> None:
+    print(f"{client.user} is now running.")
+
+# Step 4: Handling messages
+@client.event
+async def on_message(message: Message) -> None:
+    if message.author == client.user:
+        return
+    
+    username: str = str(message.author)
+    user_message: str = str(message.content)
+    channel: str = str(message.channel)
+
+    print(f"[{channel}] {username} said: '{user_message}'")
+    await send_message(message, user_message)
+
+# Step 5: Main entry point
+def main() -> None:
+    client.run(token=TOKEN)
+
+
+if __name__ == "__main__":
+    main()
--- responses.py
+++ responses.py
@@ -1,2 +1,17 @@
+from random import choice, randint
+
 def get_response(user_input: str) -> str:
-    raise NotImplementedError("Not implemented yet!")
+    lowered: str = user_input.lower()
+
+    if lowered == '':
+        return "bruh"
+    elif "hello" in lowered:
+        return "hey"
+    elif "what" in lowered:
+        return "tf u mean 'what' bitch use ur words"
+    elif "how" in lowered:
+        return "how what"
+    elif "roll da dice" in lowered:
+        return f"yeah so basically you rolled {randint(1, 6)}"
+    else:
+        return choice(["uhh, yeah idk man", "yeah i dont know what that means", "what u say"])
