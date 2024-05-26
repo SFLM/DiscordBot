0dd6494ce677299cb070c25b6cc55c5db9248008
Shan-wei <500318@student.saxion.nl>
2024-05-26 11:27:39 +0200

    Changed functionality from listening to messages to slash commands

--- main.py
+++ main.py
@@ -1,52 +1,52 @@
 from typing import Final
 import os
 from dotenv import load_dotenv
-from discord import Intents, Client, Message, app_commands
+from discord import Intents, Client, Interaction, app_commands, Object
 from responses import get_response
 
-# Step 0: Load our token from the .env file
+# Load our token from the .env file
 load_dotenv()
 TOKEN: Final = os.getenv("DISCORD_TOKEN")
 
-# Step 1: Bot setup
+# Bot setup
 intents: Intents = Intents.default()
 intents.message_content = True
 client: Client = Client(intents=intents)
+tree: app_commands.CommandTree = app_commands.CommandTree(client)
+guild_id = 235049617354391563
 
-# Step 2: Listen for messages
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        print("(Message was empty because intents were not enabled)")
-        return
-    
-    if is_private := user_message[0] == '?':
-        user_message = user_message[1:]
-    
+
+# Listen for messages/interactions
+@tree.command(
+    name = "no_args",
+    description="My application command",
+    guild = Object(id=guild_id)
+)
+async def no_args(interaction: Interaction) -> None:
     try:
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
+        await interaction.response.send_message("Hi there!")
     except Exception as e:
         print(e)
 
-# Step 3: Handling startup for bot
+@tree.command(
+    name = "with_args",
+    description="My application command",
+    guild = Object(id=guild_id)
+)
+async def with_args(interaction: Interaction, user_input: str) -> None:
+    try:
+        response: str = get_response(user_input)
+        await interaction.response.send_message(response)
+    except Exception as e:
+        print(e)
+
+# Handling startup for bot
 @client.event
 async def on_ready() -> None:
-    print(f"{client.user} is now running.")
+    await tree.sync(guild=Object(id=guild_id))
+    print(f"{client.user} is now running!!!!")
 
-# Step 4: Handling messages
-@client.event
-async def on_message(message: Message) -> None:
-    if message.author == client.user:
-        return
-    
-    username: str = str(message.author)
-    user_message: str = str(message.content)
-    channel: str = str(message.channel)
-
-    print(f"[{channel}] {username} said: '{user_message}'")
-    await send_message(message, user_message)
-
-# Step 5: Main entry point
+# Main entry point
 def main() -> None:
     client.run(token=TOKEN)
 
